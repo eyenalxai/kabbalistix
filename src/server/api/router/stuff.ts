7d10c4895d14671856dba67c9db699948e554579
Sax Dhiut <mail@iyxeyl.me>
2025-08-19 07:17:15 +0400

    refactor: remove authentication and database-related files

--- src/server/api/router/stuff.ts
+++ src/server/api/router/stuff.ts
@@ -1,26 +1,9 @@
-import { z } from "zod"
-import {
-	createTRPCRouter,
-	protectedProcedure,
-	publicProcedure
-} from "@/server/api/trpc"
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc"
 
 export const stuffRouter = createTRPCRouter({
 	getPublicStuff: publicProcedure.query(async () => {
 		return {
 			message: "Hello, world!"
 		}
-	}),
-	getProtectedStuff: protectedProcedure
-		.input(
-			z.object({
-				uuid: z.string()
-			})
-		)
-		.query(async ({ ctx, input }) => {
-			return {
-				userId: ctx.session.user.id,
-				uuid: input.uuid
-			}
-		})
+	})
 })
