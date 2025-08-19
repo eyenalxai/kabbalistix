a39c172575bb799cc253f10dce317c4a3849f454
Sax Dhiut <mail@iyxeyl.me>
2025-08-19 07:49:58 +0400

    feat: add Button and Input components, integrate Sonner for notifications, and update Providers to include Toaster

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -3,11 +3,14 @@
 import type { ThemeProviderProps } from "next-themes"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { TRPCReactProvider } from "@/components/providers/trpc-provider"
+import { Toaster } from "@/components/ui/sonner"
 
 export function Providers({ children, ...props }: ThemeProviderProps) {
 	return (
 		<NextThemesProvider {...props}>
-			<TRPCReactProvider>{children}</TRPCReactProvider>
+			<TRPCReactProvider>
+				{children} <Toaster />
+			</TRPCReactProvider>
 		</NextThemesProvider>
 	)
 }
