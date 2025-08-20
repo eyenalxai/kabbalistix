8f9c9986c4d74a3b3922069b943092d15a21ef17
Sax Dhiut <mail@iyxeyl.me>
2025-08-21 00:50:03 +0400

    chore: switch to next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,13 +1,15 @@
+import type { NextConfig } from "next"
+
 if (!process.env.NEXT_PUBLIC_APP_URL) {
 	throw new Error("NEXT_PUBLIC_APP_URL environment variable is required")
 }
 
-const extractDomain = (url) => {
+const extractDomain = (url: string) => {
 	const urlObj = new URL(url)
 	return urlObj.hostname
 }
 
-const nextConfig = {
+const nextConfig: NextConfig = {
 	transpilePackages: ["geist"],
 	allowedDevOrigins: [extractDomain(process.env.NEXT_PUBLIC_APP_URL)]
 }
